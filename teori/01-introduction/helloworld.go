f2e87beb86c0a474b4227eee8e9171030b1af640
Nabil Aqbar Kurniawijaya Putra <laffei132@gmail.com>
2026-09-22 21:32:14 +0700

    Tugas Algoritma Bola Terberat

---
+++ teori/01-introduction/helloworld.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	var umur int8
+	var anakke int8
+	var saldo int64
+
+	umur = 26
+	anakke = 1
+	saldo = 1000000000
+
+	fmt.Println("Umur saya adalah", umur)
+	fmt.Println("Anak ke:", anakke)
+	fmt.Println("Saldo saya adalah:", saldo)
+
+	
+}
